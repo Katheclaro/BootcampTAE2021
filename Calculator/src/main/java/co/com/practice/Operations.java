ed1021dc9fda6e25da638f9d4e28e78c1f5b5349
Johana Katherine Clavijo Rondon <johana.clavijo@globant.com>
2021-04-07 19:08:18 -0500

    Add multiplication and division methods

--- Calculator/src/main/java/co/com/practice/Operations.java
+++ Calculator/src/main/java/co/com/practice/Operations.java
@@ -2,6 +2,16 @@ package co.com.practice;
 
 public class Operations {
 
+    public int multiplication(int a, int b){
+        int c = a*b;
+        return c;
+    }
+
+    public int division(int a, int b) {
+        int c = a / b;
+        return c;
+    }
+
     public int sum(int a, int b){
         int c = a+b;
         return c;
